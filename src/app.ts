f06f3019d84d20154e29a24dcae2b1b75ca85e63
Leonardo Ferreira Monteiro da Silva <leonardo@tefra.com.br>
2025-08-17 19:36:43 -0300

    readme updates

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,5 @@ import { env } from "./config/env";
 const app = express();
 
 app.listen(env.port, () => {
-    console.log('Server running on port ' + env.port);
     console.log(`Server running on port ${env.port}`);
 });
